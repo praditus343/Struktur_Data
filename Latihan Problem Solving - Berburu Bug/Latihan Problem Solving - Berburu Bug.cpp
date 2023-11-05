ed273ef10934ca1dfcb289a488bd0c6182150bcb
praditus343 <88468631+praditus343@users.noreply.github.com>
2023-11-06 01:28:38 +0700

    first commit

---
+++ Latihan Problem Solving - Berburu Bug/Latihan Problem Solving - Berburu Bug.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+cout<<"Praditus Egi Danuarta_22.11.4662_IF02\n";
+
+int a,b,temp;
+temp:
+
+cout<<"Nilai Variable A dan B Setelah ditukar : \nA= ";
+cin>>a;
+cout<<"B= ";
+cin>>b;
+
+temp=a;
+a=b;
+b=temp;
+
+cout<<"\nNilai Variable A dan B Setelah ditukar: \nA= " <<a<<"\nB = "<<b<<endl;
+return 0;
+}
